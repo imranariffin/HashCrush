6b820504e772f66918460c089e8831c33ed65bd2
imranariffin <imranariffin@yahoo.com>
2016-02-21 10:31:27 -0500

    hide twitter config

--- server/app/app.py
+++ server/app/app.py
@@ -3,6 +3,7 @@ from bottle import *
 import json
 from pymongo import MongoClient
 import datetime
+from twitter_config import *
 
 client = MongoClient()
 db = client.db
@@ -13,12 +14,6 @@ global accessToken
 
 # import 
 
-
-# twitter
-consumer_key = 'v4QtNvlKHrJLq6xfu3yHUAUb4'
-consumer_secret = 's0jZ6OA8UGoM7Nmiy1NFiIMWYZo2IMveJVVkKnitExSlWco56h'
-access_token_key = '2361833802-YeVtFwL6i24UARsmnsSuGlVEmXZMF8MM9TBENn2'
-access_token_secret = 'WmpvT8eMaV5vJ49DBA6QRNYRm72mf4RCbfxjGiJQt898F'
 
 import twitter as t
 twitter = t.Api(
---
+++ server/app/twitter_config.py
@@ -0,0 +1,5 @@
+# twitter
+consumer_key = 'v4QtNvlKHrJLq6xfu3yHUAUb4'
+consumer_secret = 's0jZ6OA8UGoM7Nmiy1NFiIMWYZo2IMveJVVkKnitExSlWco56h'
+access_token_key = '2361833802-YeVtFwL6i24UARsmnsSuGlVEmXZMF8MM9TBENn2'
+access_token_secret = 'WmpvT8eMaV5vJ49DBA6QRNYRm72mf4RCbfxjGiJQt898F'
